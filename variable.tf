e89b40b06a2cc02ca8f372c89109ea8ae1ba721d
ticlacalvin <93556160+ticlacalvin@users.noreply.github.com>
2022-05-19 17:56:15 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,7 @@
+variable "aws_region" {
+  description = "The AWS region to use to create resources."
+  default     = "us-east-1"
+}
 variable "vpc-cidr" {
   default     = "10.0.0.0/16"
   description = "VPC CIDR Block"
